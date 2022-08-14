20136ad302d3759081932ad7f7eb32b096ceccdc
vladislav.kozak <vladislav.kozak@mvideo.ru>
2022-08-14 16:44:17 +0300

    init ngrx store

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { extModules } from '../build-specifics';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    extModules,
   ],
   providers: [],
   bootstrap: [AppComponent]
